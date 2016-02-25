a7cfa0490791c2e88347121098814cc456db4797
gcarvalhos <gcarvalhos.souza@gmail.com>
2016-02-25 15:03:37 -0300

    ng-src

--- flantandersStore/app.js
+++ flantandersStore/app.js
@@ -1,9 +1,39 @@
 (function(){
 	var gems =
 	[
-		{name:'Azurite',price:'2.95', description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget molestie risus. Donec non nibh mattis, venenatis odio a, mollis ex. Etiam imperdiet orci id imperdiet sodales.',canPurchase:true},
-		{name:'Pentagon Gem',price:'5.95', description:'Vivamus justo dolor, molestie sed pellentesque id, sodales id turpis. Vestibulum eu justo molestie, pulvinar ex sagittis, efficitur nibh.',canPurchase:false},
-		{name:'Crazy Gem',price:'12.95', description:'Proin nunc tellus, finibus interdum tortor quis, varius vehicula urna.',canPurchase:true}
+		{
+			name:'Azurite',
+			price:'2.95',
+			description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget molestie risus. Donec non nibh mattis, venenatis odio a, mollis ex. Etiam imperdiet orci id imperdiet sodales.',
+			canPurchase:true,
+				images:[
+					"images/gem-01.gif",
+      				"images/gem-03.gif",
+      				"images/gem-04.gif"
+				]
+		},
+		{
+			name:'Pentagon Gem',
+			price:'5.95',
+			description:'Vivamus justo dolor, molestie sed pellentesque id, sodales id turpis. Vestibulum eu justo molestie, pulvinar ex sagittis, efficitur nibh.',
+			canPurchase:false,
+				images:[
+					"images/gem-06.gif",
+      				"images/gem-07.gif",
+      				"images/gem-09.gif"
+				]
+		},
+		{
+			name:'Crazy Gem',
+			price:'12.95', 
+			description:'Proin nunc tellus, finibus interdum tortor quis, varius vehicula urna.',
+			canPurchase:true,
+				images:[
+					 "images/gem-02.gif",
+      				 "images/gem-05.gif",
+      				 "images/gem-08.gif"
+				]
+		}
 	];
 	var app =angular.module('gemStore',[]);
 	app.controller('StoreController',function(){
